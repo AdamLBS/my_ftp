2d9839ec7c357a8e97475511df24ae7e37688efd
Adam Elaoumari <adam@admlbs.fr>
2023-02-27 13:52:52 +0100

    fixed quit

--- quit.c
+++ quit.c
@@ -10,6 +10,7 @@
 void do_quit_cmd(int index, t_clients *clients)
 {
     write(clients[index].control_fd, QUITMSG, strlen(QUITMSG));
+    shutdown(clients[index].control_fd,SHUT_RDWR);
     close(clients[index].control_fd);
     clients[index].control_fd = -1;
 }
